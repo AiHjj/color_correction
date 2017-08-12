31bde1444da213f74d5c56046d718483174585bc
AiHjj <y867876453@163.com>
2017-08-12 16:47:31 +0800

    test repository for learning git

---
+++ demo.m
@@ -0,0 +1,25 @@
+clc;close all;
+
+% [chart, patch_info] = generate_chart (1704, 2272, 100, 16, 240, 16, 240, 16, 240, 8, 16);
+
+chart = load('chart.mat');
+chart = chart.chart;
+
+patch_info = load('patch_info.mat');
+patch_info = patch_info.patch_info;
+
+proc= imread('proc.JPG');     % captured by cellphone camera
+proc=double(proc);
+
+hight=size(proc,1);
+width=size(proc,2);
+
+[cal, a, fit] = color_xform (chart, 1, 1, 1704, 2272,...
+                                          patch_info , ...
+                                          proc, 1, 1, hight, width,...
+                                          1, -1);
+ 
+                                       
+figure,imshow(uint8(chart)),title('the chart')                                    
+figure,imshow(uint8(proc)),title('the original image')
+figure,imshow(uint8(cal)),title('the CCM processed image')
